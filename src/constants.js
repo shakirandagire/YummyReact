dbe218c509dbd678901f025dfb1c964ae5ce5a93
shakirandagire <ndagire.shakira@gmail.com>
2018-03-16 23:28:39 +0300

    Changes to the constant

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-// export const BASE_URL = 'https://yummy-recipes-api.herokuapp.com';
-export const BASE_URL = 'http://127.0.0.1:5000';
+atus
+export const BASE_URL = 'https://yummy-recipes-api.herokuapp.com';
+//export const BASE_URL = 'http://127.0.0.1:5000';
